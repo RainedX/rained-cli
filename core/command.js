4be58f89d632f4149be37640b13b15bde52f87cb
RainedX <1355247497@qq.com>
2021-04-22 14:38:47 +0800

    feat:完成基本骨架

--- core/command.js
+++ core/command.js
@@ -1,18 +1,18 @@
-const { handleCreateAction, handleAddAction } = require('./action.js')
-const { program } = require('commander')
+const { handleCreateAction, handleAddAction } = require('./action.js');
+const { program } = require('commander');
 
 const handleCommand = () => {
   program
-  .command('create <projectName> [options...]')
-  .description('create a new project from a template')
-  .action(handleCreateAction)
+    .command('create <projectName> [options...]')
+    .description('create a new project from a template')
+    .action(handleCreateAction);
 
   program
-  .command('addc <name>')
-  .description('add a component, eg: rain addc cascader')
-  .action((name) => {
-    handleAddAction(name, program.opts().dest || 'src/components')
-  })
-}
+    .command('addc <name>')
+    .description('add a component, eg: rain addc cascader')
+    .action((name) => {
+      handleAddAction(name, program.opts().dest || 'src/components');
+    });
+};
 
-module.exports = handleCommand
+module.exports = handleCommand;
